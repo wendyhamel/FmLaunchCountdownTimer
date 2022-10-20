8c194b834612d0c453c1f7c5c85cd9f15ff13084
wendyhamel <whamel@it4web.nl>
2022-10-20 17:15:36 +0200

    Finished responsive styling.

--- js/tailwind_config.js
+++ js/tailwind_config.js
@@ -6,12 +6,17 @@ tailwind.config = {
         extend: {
             colors: {
                 'red': 'hsl(345, 95%, 68%)',
+                'red-shaded': 'hsl(345,55%,55%)',
                 'white': 'hsl(0, 0%, 100%)',
                 'blue-gray': 'hsl(237, 18%, 59%)',
                 'blue-des': 'hsl(236, 21%, 26%)',
                 'blue-dark': 'hsl(235, 16%, 14%)',
                 'blue-black': 'hsl(234, 17%, 12%)',
                 'purple': 'hsl(271,23%,18%)',
+            },
+            boxShadow: {
+                'bold-bottom': '0 8px 3px -3px hsl(234, 17%, 12%)',
+                'bold-bottom-lg': '0 12px 3px -3px hsl(234, 17%, 12%)',
             }
         }
     }
