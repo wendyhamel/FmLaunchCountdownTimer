5c84ef41272afd67e61b61d3e7c8f9ad2d0f7c05
wendyhamel <whamel@it4web.nl>
2022-10-21 17:46:42 +0200

    Rewrote card for animation

--- js/countdown.js
+++ js/countdown.js
@@ -1,5 +1,9 @@
 window.countdownJS = function() {
     return {
-        countDown: Alpine.$persist(true),
+        // countDown: Alpine.$persist(true),
+        days: {current: 14, next: 15},
+        hours: {current: 20, next: 21},
+        minutes: {current: 24, next: 25},
+        seconds: {current: 12, next: 13},
     }
 }
--- js/tailwind_config.js
+++ js/tailwind_config.js
@@ -14,9 +14,21 @@ tailwind.config = {
                 'blue-black': 'hsl(234, 17%, 12%)',
                 'purple': 'hsl(271,23%,18%)',
             },
+            fontSize: {
+                '7.5xl': '5rem'
+            },
             boxShadow: {
                 'bold-bottom': '0 8px 3px -3px hsl(234, 17%, 12%)',
                 'bold-bottom-lg': '0 12px 3px -3px hsl(234, 17%, 12%)',
+            },
+            animation: {
+                'flip': 'flip 900ms cubic-bezier(1, 0.24, 0.65, 0.95) infinite'
+            },
+            keyframes: {
+                flip: {
+                    '0%': {transform: 'rotateX(0)'},
+                    '100%': {transform: 'rotateX(-180deg)'}
+                }
             }
         }
     }
